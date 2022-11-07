83feb16758932e2eae9d0f1cfb47f4d8b8c66315
falls <falls@falls>
2022-11-08 00:12:19 +0700

    Added overclocks base objects

---
+++ src/systems/overclocks/amds/inc/amds.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef SYSTEMS_OVERCLOCKS_AMDS_HEADER
+#define SYSTEMS_OVERCLOCKS_AMDS_HEADER
+
+#include "../../common.hpp"
+
+namespace Systems::Overclocks::AMDs
+{
+
+}
+
+#endif
---
+++ src/systems/overclocks/nvidias/src/nvidias.cpp
@@ -0,0 +1,1 @@
+#include "../inc/nvidias.hpp"
